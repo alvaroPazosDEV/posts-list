13b36cc6ee594c9622fef68506d17d36f652cf2f
Alvaro <alvaropazos1997@gmail.com>
2019-04-20 19:40:12 -0500

    First version

---
+++ src/components/PostList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
+import UserHeader from './UserHeader'
+
+class PostList extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+    renderList() {
+        return this.props.posts.map(post => {
+            return (
+                <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <UserHeader userId={post.userId} />
+                    <br />
+                </div>
+            )
+        })
+    }
+    render() {
+        console.log(this.props.posts)
+        return <div>{this.renderList()}</div>
+    }
+}
+
+const mapStateToProps = (state) => {
+    return { posts: state.posts }
+}
+
+export default connect(
+    mapStateToProps, { fetchPosts }
+)(PostList)
